5618f8bfc5ef4e2a295d62daa7511d3baa9dbc92
SMS <mackeser@amiro.team>
2025-08-16 13:00:18 +0300

    Step 3: Fix HuggingFace repository ID (remove hyphen from Omni-Avatar)

--- handler.py
+++ handler.py
@@ -71,7 +71,7 @@ def ensure_models():
                 
                 # Download the model files
                 huggingface_hub.snapshot_download(
-                    repo_id="Omni-Avatar/OmniAvatar-14B",
+                    repo_id="OmniAvatar/OmniAvatar-14B",
                     local_dir=str(model_dir),
                     local_dir_use_symlinks=False
                 )
